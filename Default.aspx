a8c34ec741df3c6704ac0913eca14c80dc756f04
Yi Shi <yi-shi@uiowa.edu>
2014-10-01 16:03:55 -0500

    salary calculator

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Hourly Wages:<br />
+        <asp:TextBox ID="rateTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Hours:<br />
+        <asp:TextBox ID="hourTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Pre-tax deductions:<br />
+        <asp:TextBox ID="pretaxTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        After-tax dedictions:<br />
+        <asp:TextBox ID="aftertaxTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="calculateButton" runat="server" Text="Calculate Net Payment" Height="30px" Width="180px" />
+        <br />
+        <br />
+        <asp:Label ID="result" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="clearButton" runat="server" Text="Clear" Width="180px" Height="30px" />
+        <br />
+        <br />
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,43 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+        'Specify constant values
+        Const preTax As Integer = 0.18
+        Const afterTax As Integer = 0.22
+
+        'Create variables to hold the values enterd by the user
+        Dim rate As Decimal = rateTextBox.Text
+        Dim hours As Decimal = hourTextBox.Text
+        Dim pretaxD As Decimal = pretaxTextBox.Text
+        Dim aftertaxD As Decimal = aftertaxTextBox.Text
+
+        Dim pretaxWage As Decimal
+        pretaxWage = rate * hours - pretaxD
+
+        Dim Netpay As Decimal
+        If pretaxWage < 500 Then
+            Netpay = pretaxWage * (1 - preTax) - aftertaxD
+        Else
+            Netpay = pretaxWage * (1 - afterTax) - aftertaxD
+        End If
+
+        result.Text = "Your net salary payment is $" & Netpay.ToString("#,####.##")
+    End Sub
+
+    Protected Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+
+        rateTextBox.Text = String.Empty
+        hourTextBox.Text = String.Empty
+        pretaxTextBox.Text = String.Empty
+        aftertaxTextBox.Text = String.Empty
+        result.Text = String.Empty
+
+
+    End Sub
+End Class
